ea664a0693b3a8d47b14b4299118deab47e33972
Martin <reinoso.martin@gmail.com>
2019-01-20 12:59:14 -0500

    FInish

---
+++ Assets/03 - Scripts/micrsocopeSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class micrsocopeSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (GameObject.Find("MUSIC"))
+        {
+            Destroy(GameObject.Find("MUSIC"));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/CuesDoneCheck.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CuesDoneCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponentsInChildren<cue>().All(c => c.TriggerCompleted))
+        {
+            GetComponent<AudioSource>().Play();
+            GetComponent<changeScene>().loadScene = true;
+        }
+	}
+}
