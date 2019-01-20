af8acfca6013d538e465ed439e7cd928c220b587
Martin <reinoso.martin@gmail.com>
2019-01-19 21:19:02 -0500

    Added Intro scene and books in lab

---
+++ Assets/03 - Scripts/AudioOnPickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOnPickup : OVRGrabbable {
+
+
+
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
+    {
+        Debug.Log("start Grabbing");
+        base.GrabBegin(hand, grabPoint);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/03 - Scripts/AutoChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoChangeScene : MonoBehaviour {
+
+    public float timeToChange = 5;
+
+    private float timer;
+	// Use this for initialization
+	void Start () {
+        timer = timeToChange;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer = timer - Time.deltaTime;
+        if (timer < 0)
+        {
+            GetComponent<changeScene>().loadScene= true;
+        }
+	}
+}
