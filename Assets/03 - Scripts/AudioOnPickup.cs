9da18a8bd4ab11e6d6e3cfcadb3f4ea49dd789db
Martin <reinoso.martin@gmail.com>
2019-01-19 23:17:20 -0500

    added intro scenes and new microscope inside

--- Assets/03 - Scripts/AudioOnPickup.cs
+++ Assets/03 - Scripts/AudioOnPickup.cs
@@ -9,9 +9,15 @@ public class AudioOnPickup : OVRGrabbable {
     public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
     {
         Debug.Log("start Grabbing");
+        GetComponent<AudioSource>().Play(0);
         base.GrabBegin(hand, grabPoint);
     }
 
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        GetComponent<AudioSource>().Stop();
+        base.GrabEnd(linearVelocity, angularVelocity);
+    }
 
     // Use this for initialization
     void Start () {
